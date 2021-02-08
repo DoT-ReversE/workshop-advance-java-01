f7a6f89a0cf09609351399300bc6b1c0c842e798
chanon.cho <chanon.cho@KB-19LBK10B5D.kasikornbank.com>
2021-02-08 11:47:18 +0700

    Create Interface for CircularBuffer

--- workshop01/src/main/java/workshop01/CircularBuffer.java
+++ workshop01/src/main/java/workshop01/CircularBuffer.java
@@ -1,5 +1,13 @@
 package workshop01;
 
-public class CircularBuffer {
+public interface CircularBuffer {
+
+	public void create();
+	public void create(int size);
+	public void write(String input);
+	public String read();
+	public int getSize();
+	public boolean isEmpty();
+	public boolean isFull();
 
 }
---
+++ workshop01/src/main/java/workshop01/CircularBufferImpl.java
@@ -0,0 +1,34 @@
+package workshop01;
+
+public class CircularBufferImpl implements CircularBuffer {
+
+	public void create() {
+		// TODO Auto-generated method stub
+		
+	}
+	public void create(int size) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void write(String input) {
+		// TODO Auto-generated method stub
+		
+	}
+	public String read() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public int getSize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
