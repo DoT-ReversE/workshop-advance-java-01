df2967e7f9a5d43cf1f0959974b83822cfeb96b4
chanon.cho <chanon.cho@KB-19LBK10B5D.kasikornbank.com>
2021-02-09 10:33:15 +0700

    Initial project from P'Somkiat

---
+++ workshop02/src/main/java/workshop02/DiscountCalculator.java
@@ -0,0 +1,9 @@
+package workshop02;
+
+public class DiscountCalculator {
+
+    public static int get(Basket basket) {
+        // Logic
+        return 0;
+    }
+}
---
+++ workshop02/src/main/java/workshop02/PriceCalculator.java
@@ -0,0 +1,10 @@
+package workshop02;
+
+public class PriceCalculator {
+
+    public static int get(Basket basket) {
+        // Logic
+        return 0;
+    }
+    
+}
