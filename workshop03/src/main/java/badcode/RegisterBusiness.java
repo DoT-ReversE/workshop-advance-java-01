e6b7e02e1437407e7f7b3a9db5130feb35b77fb1
chanon.cho <chanon.cho@KB-19LBK10B5D.kasikornbank.com>
2021-02-09 15:35:58 +0700

    Add test about registerFee.

--- workshop03/src/main/java/badcode/RegisterBusiness.java
+++ workshop03/src/main/java/badcode/RegisterBusiness.java
@@ -13,27 +13,34 @@ public class RegisterBusiness {
         if (speaker.getFirstName() != null && !speaker.getFirstName().trim().equals("")) {
             if (speaker.getLastName() != null && !speaker.getLastName().trim().equals("")) {
                 if (speaker.getEmail() != null && !speaker.getEmail().trim().equals("")) {
-                    String emailDomain = speaker.getEmail().split("@")[1];
-                    if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
-                        int exp = speaker.getExp();
-                        if (exp <= 1) {
-                            speaker.setRegistrationFee(500);
-                        } else if (exp >= 2 && exp <= 3) {
-                            speaker.setRegistrationFee(250);
-                        } else if (exp >= 4 && exp <= 5) {
-                            speaker.setRegistrationFee(100);
-                        } else if (exp >= 6 && exp <= 9) {
-                            speaker.setRegistrationFee(50);
-                        } else {
-                            speaker.setRegistrationFee(0);
-                        }
-                        try {
-                            speakerId = repository.saveSpeaker(speaker);
-                        }catch (Exception exception) {
-                            throw new SaveSpeakerException("Can't save a speaker.");
-                        }
+                	// TODO :: 
+                	// Fix IdxOutOfBound
+                    String[] splitEmailWithAssign = speaker.getEmail().split("@");
+                    if (splitEmailWithAssign.length == 2) {
+                    	String emailDomain = splitEmailWithAssign[1];
+                    	if (Arrays.stream(domains).filter(it -> it.equals(emailDomain)).count() == 1) {
+                    		int exp = speaker.getExp();
+                    		if (exp <= 1) {
+                    			speaker.setRegistrationFee(500);
+                    		} else if (exp >= 2 && exp <= 3) {
+                    			speaker.setRegistrationFee(250);
+                    		} else if (exp >= 4 && exp <= 5) {
+                    			speaker.setRegistrationFee(100);
+                    		} else if (exp >= 6 && exp <= 9) {
+                    			speaker.setRegistrationFee(50);
+                    		} else {
+                    			speaker.setRegistrationFee(0);
+                    		}
+                    		try {
+                    			speakerId = repository.saveSpeaker(speaker);
+                    		}catch (Exception exception) {
+                    			throw new SaveSpeakerException("Can't save a speaker.");
+                    		}
+                    	} else {
+                    		throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
+                    	}
                     } else {
-                        throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
+                    	throw new SpeakerDoesntMeetRequirementsException("Speaker doesn't meet our standard rules.");
                     }
                 } else {
                     throw new ArgumentNullException("Email is required.");
